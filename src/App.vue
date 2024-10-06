f0b7d66270fffcf111302731afb6278e3ab7b0a0
Anurag Modi <anuragmodi02@yahoo.com>
2024-10-07 02:40:25 +0530

    Bug fix attempt

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,3 @@ export default {
   name: 'App',
 }
 </script>
-
-<style>
-@font-face {
-  font-family: 'MyWebFont';
-  src: url('../src/assets/fonts/Rubik-VariableFont_wght.ttf');
-}
-
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-color: #313E51;
-  height: 100%;
-}
-</style>
